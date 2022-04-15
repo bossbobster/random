6339f8807af00ad51505d8a7d5a1e5bc4cfdf737
BossBobster <34829413+bossbobster@users.noreply.github.com>
2022-04-14 18:22:51 -0700

    Updated the word list

--- IllegalWordChecker.cpp
+++ IllegalWordChecker.cpp
@@ -1,4 +1,4 @@
-string illegal[] = {"the reader", "situation", "illustrate", "through", "interesting", "simple", "simply", " show", "display", "vividly", "directly", " big ", " huge", " large", "lifestyle", "mindset", "pure", "goes to new heights", "amazing", "impact", "effective", "off of", "plot", "to deal with", " me ", " my ", " you", " i ", " we ", " our ", " one ", " us ", " goes into", "confusing", "helps to understand", "later in the novel", "explain", " be ", " is ", " was ", " are ", " were ", " becom", "able to", " very significant ", "attitude", "the book", "to not", "constantly", "go through", "up to", "continuously", "go against", "goes against", "went against", "to put up with", "heavily", "extremely", "in order", "based on", "revolve around", "revolves around", "revolved around", "whilst", " on ", " where "};
+string illegal[] = {"the reader", "situation", "illustrate", "through", "interesting", "simple", "simply", " show", "display", "vividly", "directly", " big ", " huge", " large", "lifestyle", "mindset", "pure", "new heights", "amazing", "impact", "effective", "off of", "plot", "to deal with", " me ", " my ", " you", " i ", " we ", " our ", " one ", " us ", " goes into", "go into", "went into", "confus", "helps to understand", "later in the novel", "explain", " be ", " is ", " was ", " are ", " were ", " becom", "able to", "very significant", "attitude", "the book", "to not", "constantly", "go through", "up to", "continuously", "go against", "goes against", "went against", "put up with", "puts up with", "heavily", "extremely", "in order", "based on", "revolve around", "revolves around", "revolved around", "whilst", " on ", " where ", "however", "base itself", "base oneself", "base themselves", "primarily"};
 int main()
 {
     string word, essay =
@@ -20,7 +20,7 @@ int main()
         }
             
     }
-    cout << edited;
+    cout << edited << '\n';
     for(auto it : illegal)
         if(edited.find(it) != string::npos)
             cout << it << '\n';
